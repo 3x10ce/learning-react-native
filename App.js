adc6a6027171f2e77f6fe987843408fa247e3dfd
3x10ce <hiro.h.324@gmail.com>
2017-11-24 14:11:06 +0900

    Style

--- App.js
+++ App.js
@@ -1,46 +1,53 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-class Blink extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {showText: true};
-
-    // Toggle the state every second
-    setInterval(() => {
-      this.setState(previousState => {
-        return {showText: !previousState.showText};
-      })
-    }, 1000);
-  }
-
-  render () {
-    let display = this.state.showText ? this.props.text : '';
-    return (
-      <Text>{display}</Text>
-    )
-    
-  }
-}
-
-export default class BlinkApp extends Component {
+export default class LotsOfStyles extends Component {
   render () {
     return (
       <View>
-        <Blink text="I love to blink!" />
-        <Blink text="Yes blinking is so great!" />
-        <Blink text="コンポーネントにはpropsとstateという2つのタイプがあります。" />
-        <Blink text="propsは親によってセットされ、これらはコンポーネントのライフサイクルに固定されます。" />
-        <Blink text="データを変更する予定がある場合には、stateを使います。" />
-        <Blink text="一般的に、コンストラクタでstateを初期化し、stateに変更を加える時はsetStateを用いるべきです。" />
-        <Blink text="例えば、常にチカチカするテキストを作りたいとしましょう。テキストはチカチカコンポーネントを生み出した時に1回セットし、それはpropsに入ります。" />
-        <Blink text="「今この時、テキスト表示はONかOFFか」は時間で変化しますので、この情報はstateに保持するべきです。" />
-        <Blink text="" />
-        <Blink text="" />
-        <Blink text="" />
-        </View>
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+        <Text style={styles.red}></Text>
+        <Text style={styles.red}>React Nativeを利用するにあたり、あなたは特別なスタイル定義構文を覚える必要はありません。</Text>
+        <Text style={styles.bold}>アプリケーションのスタイルもJavaScriptのみで設定できます。</Text>
+        <Text style={[]}>すべてのコンポーネントはstyleプロパティを受け取ることができます。</Text>
+        <Text style={[styles.red, styles.bold]}>スタイル名と値の指定は、名前の指定がキャメルケースであることを除いて、ほとんどがWebにおけるCSSの指定方法と一致します。</Text>
+        <Text style={styles.bold}>（たとえば、background-colorではなく、backgroundColor）</Text>
+        <Text style={styles.red}></Text>
+        <Text style={{color: 'green'}}>styleプロパティはプレーンで古いJavaScriptオブジェクトです。</Text>
+        <Text style={{fontSize: 24}}>それはもっともシンプルであり、我々がサンプルコードを書く際によく用いるものです。</Text>
+        <Text style={[{color: 'green'}, {fontWeight: 'bold'}]}>複数のスタイルを配列で指定することもできます。</Text>
+        <Text style={[{color: 'orange'}, {color: 'purple'}]}>その場合、配列の後ろに書いたスタイルの方が優先順位を持ち、前に書いたスタイルを上書きします。</Text>
+        <Text style={styles.red}>コンポーネントが複雑になるにつれ、通常はStyleSheet.createを用いていくつかのスタイル定義を1箇所で定義する方法が用いられます。</Text>
+        <Text style={[]}></Text>
+        <Text style={[]}>一般的なパターンのひとつは、メインコンポーネントで受け取ったstyleプロパティをサブコンポーネントのスタイル定義するために使いまわすことです。</Text>
+        <Text style={[]}>これは、CSSで用いられた「階層的な」スタイル定義を実現することができます。</Text>
+        <Text style={[]}>ここで挙げた以外にも、テキストのスタイルを定義するたくさんの方法が用意されています。すべてのスタイルのリストを知りたい人は、Text component referenceを読んで見てくださいね。</Text>
+        <Text style={[]}></Text>
+        <Text style={[]}>これであなたは、テキストを美しくスタイリングする技術を習得しました。</Text>
+        <Text style={[]}>スタイルマスターになるための次のステップでは、コンポーネントの大きさを制御する方法を学びます。</Text>
+        <Text style={[]}></Text>
+        
+      </View>
     );
   }
 }
 
-AppRegistry.registerComponent('LearningReactNative', () => BlinkApp);
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30
+  },
+  red: {
+    color: 'red'
+  },
+  bold: {
+    fontWeight: 'bold'
+  }
+});
+
+
+AppRegistry.registerComponent('LearningReactNative', () => LotsOfStyles);
