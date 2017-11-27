a6adf2df16b347c66263bbac457db3acdc741420
3x10ce <hiro.h.324@gmail.com>
2017-11-27 18:52:31 +0900

    Layout with Flexbox - AlignItems

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
 import React, {Component} from 'react';
 import {AppRegistry, View} from 'react-native';
+
 /**
- * justifyContentを追加したコンポーネントは、子要素の主軸に沿ったdistoribution(分布)を決定します。
- * 子要素たちは先頭、中央、末尾、均等配置のどれに沿って配置されるべきでしょうか？
- * 利用可能なオプションは `flex-start`, `center`, `flex-end`, `space-around`, そして`space-between`です。
+ * alignItemsを追加したコンポーネントは、secondary axis(副軸)に沿った子要素の整列方法を決定します。
+ * (たとえば、主軸を'row'としたときの副軸は'column'になります。)
+ * 子要素は先頭・中央・末尾、そしてコンポーネントいっぱいに伸長して配置、どのように配置されるべきでしょうか？
+ * 利用可能なオプションは'flex-start'、'center', 'flex-end', そして'stretch'です。
+ * 
+ * stretchが効果を発揮するためには、子要素は副軸に対しての固定幅による寸法を持ってはいけません。
+ * 以下の例では、alignItems: stretch の指定は子要素から width: 50 を取り除くまで動作しません。
+ * 
+ * これで基本的な部分は網羅しましたが、この他にもあなたが必要とするであろうたくさんのスタイルが存在します。
+ * レイアウトを制御するすべてのプロパティは、以下のドキュメントに掲載されています。
+ * https://facebook.github.io/react-native/docs/layout-props.html
+ * 
+ * 実際のアプリケーション構築に一歩近づいています。
+ * 欠けている内容の一つは、ユーザからの入力を得る手段です。
+ * さあ、TextInputコンポーネントを用いてテキスト入力を制御する手段を学びましょう！
  */
-export default class JustifyContentBasics extends Component {
+export default class AlignItemsBasics extends Component {
   render () {
     return (
-      // `justifyContent`の設定を`center`に変えてみよう
+      // `alignItems`の設定を`flex-start`に変えてみよう
+      // `justifyContent`の設定を`flex-end`に変えてみよう
       // `flexDirection`の設定を`row`に変えてみよう
       <View style={{
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'space-between'
+        justifyContent: 'center',
+        alignItems: 'center'
       }}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
@@ -24,4 +39,4 @@ export default class JustifyContentBasics extends Component {
 }
 
 // Create React Native App を利用している場合は、以下をスキップします。
-AppRegistry.registerComponent('LearningReactNative', () => JustifyContentBasics);
+AppRegistry.registerComponent('LearningReactNative', () => AlignItemsBasics);
