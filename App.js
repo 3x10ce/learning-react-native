65e6931d63fcc88fd929a44b2c5716297b4acb62
3x10ce <hiro.h.324@gmail.com>
2017-11-27 12:58:57 +0900

    Layout with Flexbox - Flex Direction

--- App.js
+++ App.js
@@ -2,29 +2,33 @@ import React, {Component} from 'react';
 import {AppRegistry, View} from 'react-native';
 
 /**
- * コンポーネントスタイルに"flex"を使用すると、コンポーネントが使えるスペースの大きさに基づいて大きさを伸ばしたり、縮めたりすることができます。
- * 通常は `flex: 1`を使うでしょう。こうすると親コンポーネントの領域いっぱいに、同じ親に属するコンポーネント間で均等な高さになるように調整されます。
- * より大きなflexが与えられると、兄弟コンポーネント間のflex値の比に基づいてより高い比率のスペースを持つようになります。
+ * コンポーネントは、flexboxを使用して
+ * コンポーネントの子に対してレイアウトを指定することがができます。
+ * Flexboxは異なる画面サイズに対して一貫したレイアウトを提供できるよう設計されています。
+ * 正しいレイアウトを実現するためには、flexDimension, alignItems, そして
+ * justifyContent を組み合わせて使用することが一般的です。
  * 
- * ( 親コンポーネントの高さが0より大きい時、子コンポーネントは親コンポーネントの領域いっぱいまでしか広げられません。
- *   もし親コンポーネントの寸法がwidth/heightによる指定、flexによる指定のいずれも使っていない場合、
- *   親コンポーネントの寸法は0となってしまい、子コンポーネントは表示されなくなります。)
+ * Flexboxは、いくつかの例外はありますがWebにおけるCSSと同じような働きをReact Native上で行います。
+ * デフォルト値は異なっており、flexDirectionでは`row`ではなく`column`、
+ * そして`flex`のパラメータは単一の数値のみがサポートされています。
  * 
- * コンポーネントサイズの調整方法を覚えた次のステップでは、画面レイアウトの方法を学びます。
+ * flexDimensionを追加したコンポーネントのスタイルは、
+ * そのレイアウトの**primary axis**(主軸)を決定します。
+ * 子要素は、水平方向('row')と垂直方向('column')のどちらに整列すべきでしょうか？
+ * そのデフォルトは 'column'です。
  */
-export default class FlexDimensionsBasics extends Component {
+export default class FlexDirectionBasics extends Component {
   render () {
     return (
-      // ここの親ビューに書かれてる `flex: 1`を消して見てね。
-      // 親ビューに寸法定義がなくなると、子ビューは親ビューに合わせた伸長ができなくなるよ。 
-      // `flex: 1`の代わりに、 `height: 300` としてみると、どうなるかな？
-      <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      // `flex Direction`の設定項目を`column`に変えてみてね
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
 }
 
-AppRegistry.registerComponent('LearningReactNative', () => FlexDimensionsBasics);
+// もしCreate React Native Appを使っているなら、この行は飛ばしてね
+AppRegistry.registerComponent('LearningReactNative', () => FlexDirectionBasics);
