5951c5cfb4741a8b392fbeb8b0fe74398d819b7d
3x10ce <hiro.h.324@gmail.com>
2017-11-27 18:33:43 +0900

    Layout with Flexbox - Justify Content

--- App.js
+++ App.js
@@ -1,27 +1,20 @@
 import React, {Component} from 'react';
 import {AppRegistry, View} from 'react-native';
-
 /**
- * コンポーネントは、flexboxを使用して
- * コンポーネントの子に対してレイアウトを指定することがができます。
- * Flexboxは異なる画面サイズに対して一貫したレイアウトを提供できるよう設計されています。
- * 正しいレイアウトを実現するためには、flexDimension, alignItems, そして
- * justifyContent を組み合わせて使用することが一般的です。
- * 
- * Flexboxは、いくつかの例外はありますがWebにおけるCSSと同じような働きをReact Native上で行います。
- * デフォルト値は異なっており、flexDirectionでは`row`ではなく`column`、
- * そして`flex`のパラメータは単一の数値のみがサポートされています。
- * 
- * flexDimensionを追加したコンポーネントのスタイルは、
- * そのレイアウトの**primary axis**(主軸)を決定します。
- * 子要素は、水平方向('row')と垂直方向('column')のどちらに整列すべきでしょうか？
- * そのデフォルトは 'column'です。
+ * justifyContentを追加したコンポーネントは、子要素の主軸に沿ったdistoribution(分布)を決定します。
+ * 子要素たちは先頭、中央、末尾、均等配置のどれに沿って配置されるべきでしょうか？
+ * 利用可能なオプションは `flex-start`, `center`, `flex-end`, `space-around`, そして`space-between`です。
  */
-export default class FlexDirectionBasics extends Component {
+export default class JustifyContentBasics extends Component {
   render () {
     return (
-      // `flex Direction`の設定項目を`column`に変えてみてね
-      <View style={{flex: 1, flexDirection: 'row'}}>
+      // `justifyContent`の設定を`center`に変えてみよう
+      // `flexDirection`の設定を`row`に変えてみよう
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+      }}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
@@ -30,5 +23,5 @@ export default class FlexDirectionBasics extends Component {
   }
 }
 
-// もしCreate React Native Appを使っているなら、この行は飛ばしてね
-AppRegistry.registerComponent('LearningReactNative', () => FlexDirectionBasics);
+// Create React Native App を利用している場合は、以下をスキップします。
+AppRegistry.registerComponent('LearningReactNative', () => JustifyContentBasics);
